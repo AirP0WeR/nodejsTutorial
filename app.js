29951f464c27304f9b16791b6ed5c9d426803a89
AirP0WeR <mgyp@mail.ru>
2023-01-06 19:58:17 +0300

    end

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-import tutorial from "./tutorial.mjs"; 
+import express from "express";
+import url from 'url';
+import path from 'path';
 
-console.log(tutorial);
-console.log(tutorial.sum(1,2));
-console.log(tutorial.PI);
 
-new tutorial.SomeMathobject();
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = 3000;
+
+app.use('/public', express.static(path.join(__dirname, 'static')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+    // res.send("Hello World!");
+});
+
+app.get("/example", (req, res) => {
+    res.send("hitting example rout!");
+});
+
+app.get('/example/:name/:age', (req, res) =>{
+    console.log(req.params);
+    console.log(req.query);
+       
+    res.send(req.params.name + " : " + req.params.age);
+});
+
+
+app.listen(port, ()=> {
+    console.log("Server is running on port 3000");
+});
---
+++ app3.js
@@ -0,0 +1,7 @@
+import tutorial from "./tutorial.mjs"; 
+
+console.log(tutorial);
+console.log(tutorial.sum(1,2));
+console.log(tutorial.PI);
+
+new tutorial.SomeMathobject();
